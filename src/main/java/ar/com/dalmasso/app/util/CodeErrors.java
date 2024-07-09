b003e086eb11752face2a21de838f9c19c28f5dc
HugoLucero <hugolucerofotografo@gmail.com>
2024-07-08 21:35:16 -0300

    Agrego handler y enum errores

---
+++ src/main/java/ar/com/dalmasso/app/util/CodeErrors.java
@@ -0,0 +1,12 @@
+package ar.com.dalmasso.app.util;
+
+public enum CodeErrors {
+    TOKEN_NOT_EQUAL,
+    USER_NOT_FOUND,
+    USERNAME_NULL_EMPTY,
+    PASSWORD_EMPTY,
+    TOKEN_INVALID,
+    ROLES_NULL_EMPTY,
+    ROL_NOT_FOUND,
+    TOKEN_INCORRECT
+}
---
+++ src/main/java/ar/com/dalmasso/app/util/ErrorHandler.java
@@ -0,0 +1,9 @@
+package ar.com.dalmasso.app.util;
+
+public class ErrorHandler extends Exception{
+
+
+    public ErrorHandler(String message) {
+        super(message);
+    }
+}
